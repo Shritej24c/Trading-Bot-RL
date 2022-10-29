a07b4feb81c89e57465ccecaa28bacdbbbc0ec8a
Shritej Shrikant Chavan <shritej24c@gmail.com>
2022-10-28 23:42:59 -0500

    Practicing some Mini RL projects with the pre-defined environments

---
+++ Mini RL projects.py
@@ -0,0 +1,19 @@
+import gym
+from stable_baselines3 import DQN, A2C, PPO, DDPG
+
+env = gym.make("LunarLander-v2")
+
+
+print(env.action_space.sample())
+print(env.observation_space.sample())
+
+model = A2C("MlpPolicy", env, verbose = 1)
+model.learn(total_timesteps=10000)
+
+state = env.reset()
+while True:
+    action, _states = model.predict(state)
+    obs, reward, done, info = env.step(action)
+    env.render(mode='human')
+
+
